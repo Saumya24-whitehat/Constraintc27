2c3c3b2299bfa6f99bd088c8cc5c57ce1ee8177b
Saumya24-whitehat <saumya2427130164@gmail.com>
2021-01-13 21:46:22 +1030

    Constraint

---
+++ Chain.js
@@ -0,0 +1,17 @@
+class Chain {
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    display(){
+        var pointa=this.chain.bodyA.position;
+        var pointb=this.chain.bodyB.position;
+        line(pointa.x,pointa.y,pointb.x,pointb.y);
+    }
+}
